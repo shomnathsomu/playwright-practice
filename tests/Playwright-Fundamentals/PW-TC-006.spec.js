4648352b6f2ba5a909e40204c873bb3a84a48e97
shomnathsomu <shomnathcse22@gmail.com>
2026-09-15 16:51:29 +0600

    Enter Text into an Input

---
+++ tests/Playwright-Fundamentals/PW-TC-006.spec.js
@@ -0,0 +1,36 @@
+// Practice Test Case #6 — Enter Text into an Input
+import { test, expect } from '@playwright/test';
+
+test.use({
+  viewport: {
+    height: 768,
+    width: 1366
+  },
+
+  // Wait 1 second after each Playwright operation
+  launchOptions:{
+    slowMo: 1000
+  }
+});
+
+// Increase timeout because slowMo makes the test take longer
+test.beforeEach(async () => {
+  test.setTimeout(120_000);
+});
+
+test('Enter username into login field', async ({ page }) => {
+
+  // 1. Navigate to SauceDemo
+  await page.goto('https://www.saucedemo.com/');
+
+  // 2. Locate username field
+  const usernameInput = page.getByRole('textbox', {
+    name: 'Username'
+  });
+
+  // 3. Enter username
+  await usernameInput.fill('standard_user');
+
+  // 4. Verify entered value
+  await expect(usernameInput).toHaveValue('standard_user');
+});
